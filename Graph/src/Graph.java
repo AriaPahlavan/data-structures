e538dc2c1f13368ff32acd0f50d6e414e55e6854
Aria Pahlavan <apahlavan1@utexas.edu>
2018-03-19 22:53:21 -0500

    implement pair and a JUnit test for graph

--- Graph/src/Graph.java
+++ Graph/src/Graph.java
@@ -1,3 +1,5 @@
+// Aria Pahlavan on March 19 2018
+
 public class Graph {
 
     /**
@@ -6,6 +8,15 @@ public class Graph {
      * @param V number of vertices in the graph
      */
     public Graph(int V) {
+    }
+
+    /**
+     * Create an empty graph with {@code V} vertices
+     *
+     * @param V number of vertices in the graph
+     * @param edges list of edges in the graph
+     */
+    public Graph(int V, Pair<Integer, Integer>[] edges) {
     }
 
     /**
@@ -22,22 +33,62 @@ public class Graph {
      * @param v the vertex whose adjacency list is being retrieved
      * @return vertices adjacent to {@code v}
      */
-    Iterable<Integer> adjOf(int v) {
+    public Iterable<Integer> adjOf(int v) {
         return null;
     }
 
     /**
      * @return number of vertices in the graph
      */
-    int V() {
+    public int V() {
         return 0;
     }
 
     /**
      * @return number of edged in the graph
      */
-    int E() {
+    public int E() {
         return 0;
+    }
+
+    /**
+     * @return the degree of vertex {@code v}
+     */
+    public int degreeOf(int v) {
+        int degree = 0;
+        for (Integer ignored : adjOf(v)) degree++;
+
+        return degree;
+    }
+
+    /**
+     * @return maximum degree
+     */
+    public int maxDegree() {
+        int max = 0, curDegree;
+
+        for (int v = 0; v < this.V(); v++)
+            if ((curDegree = degreeOf(v)) > max)
+                max = curDegree;
+
+        return max;
+    }
+
+    /**
+     * @return average degree
+     */
+    public double averageDegree() {
+        return 2.0 * this.E() / this.V();
+    }
+
+    public int numberOfSelfLoop() {
+        int count = 0;
+
+        for (int v = 0; v < this.V(); v++)
+            for (Integer u : adjOf(v))
+                if (v == u) count++;
+
+        return count/2; //each edge is considered twice
     }
 
     @Override
--- Graph/src/GraphTest.java
+++ Graph/src/GraphTest.java
@@ -1,12 +1,42 @@
-import static org.junit.Assert.*;
+// Aria Pahlavan on March 19 2018
+
+import org.junit.*;
+
 
 public class GraphTest {
 
-    @org.junit.Before
+    @Before
     public void setUp() throws Exception {
     }
 
-    @org.junit.After
+    @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testGraph() {
+        int V = 13, E = 13;
+
+        Pair<Integer, Integer>[] edges = (Pair<Integer, Integer>[]) new Pair[]{
+                Pair.of(0, 5),
+                Pair.of(4, 3),
+                Pair.of(0, 1),
+                Pair.of(9, 12),
+                Pair.of(6, 4),
+                Pair.of(5, 4),
+                Pair.of(0, 2),
+                Pair.of(11, 12),
+                Pair.of(9, 10),
+                Pair.of(0, 6),
+                Pair.of(7, 8),
+                Pair.of(9, 11),
+                Pair.of(5, 3)
+        };
+
+        Graph g = new Graph(V, edges);
+
+        for (int v = 0; v < g.V(); v++)
+            for (Integer u : g.adjOf(v))
+                System.out.println(v + "-" + u);
     }
 }
