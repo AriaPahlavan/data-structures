57a1893d212ee0ec5d9315281017ae9059a9fec1
Aria Pahlavan <apahlavan1@utexas.edu>
2018-03-24 20:50:00 -0500

    Implement connected component API

--- Graph/src/ConnectedComponents.java
+++ Graph/src/ConnectedComponents.java
@@ -1,11 +1,39 @@
 // Aria Pahlavan on March 22 2018
 
-public class ConnectedComponents {
+public class ConnectedComponents{
+    private final boolean[] marked;
+    private final int[] id;
+    private int count;
+
     /**
      * find connected components in G
      * @param G graph
      */
     public ConnectedComponents(Graph G) {
+        int V = G.V();
+        marked = new boolean[G.V()];
+        id = new int[G.V()];
+        count = 0;
+
+        for (int v = 0; v < V; v++)
+            if (!marked[v]) {
+                dfs(G, v);
+                count++;
+            }
+    }
+
+    /**
+     * all vertices found in the same call to dfs and the same id
+     * @param G graph
+     * @param v vertex
+     */
+    private void dfs(Graph G, int v) {
+        marked[v] = true;
+        id[v] = count;
+
+        for (Integer u : G.adjOf(v))
+            if (!marked[u])
+                dfs(G, u);
     }
 
     /**
@@ -15,14 +43,14 @@ public class ConnectedComponents {
      * false otherwise
      */
     public boolean isConnected(int u, int v) {
-        return false;
+        return id[u] == id[v];
     }
 
     /**
      * @return number of connected components
      */
     public int count() {
-        return 0;
+        return count;
     }
 
     /**
@@ -30,6 +58,6 @@ public class ConnectedComponents {
      * @return component identifier for {@code v}
      */
     public int id(int v) {
-        return 0;
+        return id[v];
     }
 }
