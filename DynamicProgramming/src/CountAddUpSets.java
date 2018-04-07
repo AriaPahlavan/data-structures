85524b688dcff3b773e512234f0ad83f4547174e
Aria Pahlavan <apahlavan1@utexas.edu>
2018-04-07 15:02:30 -0500

    Implement 'count sets adding up to total' recursive DP algorithm

---
+++ DynamicProgramming/src/CountAddUpSets.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountAddUpSets {
+    public static long of(long[] possibilities, long total) {
+        return rec(possibilities, total, possibilities.length-1, new HashMap<>());
+    }
+
+    private static long rec(long[] possibilities, long total, int i, Map<String, Long> memo) {
+        String key = total + "-" + i;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        if (total == 0) return 1;
+        if (total < 0)  return 0;
+        if (i < 0)      return 0;
+
+        long result = rec(possibilities, total, i-1, memo)
+                      + rec(possibilities, total - possibilities[i], i-1, memo);
+
+        memo.put(key, result);
+        return result;
+     }
+
+    public static void main(String[] args) {
+        if (of(new long[]{1, 2, 4, 6, 10, 5, 16}, 16) != 5)
+            System.err.println("FAIL");
+        else
+            System.out.println("PASS");
+    }
+}
